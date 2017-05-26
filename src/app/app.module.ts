82f19c63cd02a5536b11f17aea6f2fdb9cf4e934
sergdoren <sergdoren@mail.ru>
2017-05-27 01:00:06 +0300

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,6 +11,13 @@ import { MenuRightComponent } from './menu-right/menu-right.component';
 import { ContentComponent } from './content/content.component';
 import { ServicesComponent } from './content/services/services.component';
 import { AdvertisementComponent } from './shared-menu/advertisement/advertisement.component';
+import { SupportComponent } from './support/support.component';
+import { MainComponent } from './main/main.component';
+
+const routers = [
+  {path: '', component: SupportComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   declarations: [
@@ -19,13 +27,16 @@ import { AdvertisementComponent } from './shared-menu/advertisement/advertisemen
     MenuRightComponent,
     ContentComponent,
     ServicesComponent,
-    AdvertisementComponent
+    AdvertisementComponent,
+    SupportComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
